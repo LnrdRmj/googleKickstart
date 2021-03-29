39d9e6ca1a81e4a5aa733f10ee36476c99a3f121
Leoardo Ramaj <leonardo.ramaj@benefind.it>
2021-03-29 11:41:37 +0200

    L Shaped Plots primo caso

--- L Shaped Plots/app.js
+++ L Shaped Plots/app.js
@@ -20,6 +20,8 @@ for(let i = 1; i <= T; i++){
             matr[j][z] = +line[z];
     }
 
+    cont = 0;
+
     console.log(`Case #${i}: ${solve(matr)}`);
 
 }
@@ -27,14 +29,19 @@ for(let i = 1; i <= T; i++){
 function solve(matr){
 
   // Solution goes here
-  console.log(matr);
+  //console.log(matr);
 
-  for (let i = 0; i < matr.length; ++i)
-  for (let j = 0; j < matr[0].length; ++j){
-        findL(matr, 0, 0,  0, -1);
-        findL(matr, 0, 0,  1,  0);
-        findL(matr, 0, 0,  0,  1);
-        findL(matr, 0, 0, -1,  0);
+  // for (let i = 0; i < matr.length; ++i)
+  //     for (let j = 0; j < matr[0].length; ++j)
+  //         console.log(matr[i][j]);
+
+    for (let y = 0; y < matr.length; ++y)
+        for (let x = 0; x < matr[0].length; ++x){
+            console.log("Sto analizzando: " + y + " - " + x + " [" + matr[y][x] + "]");
+            findL(matr, x, y,  0, -1); //sopra
+            findL(matr, x, y,  1,  0); //sinistra
+            findL(matr, x, y,  0,  1); //sotto
+            findL(matr, x, y, -1,  0); //destra
     }
 
   //################################
@@ -45,41 +52,48 @@ function solve(matr){
 
 function findL(matr, x, y, dirX, dirY){
 
-    if (matr[y + dirY] == null || matr[y + dirY][x + dirX] == null) return;
-
     let length = 0;
 
-    while(y + dirY < matr.length && x + dirX < matr[y + dirY].length && matr[y + dirY][x + dirX] && matr[y + dirY][x + dirX] == 1) {
+    while(y < matr.length && y >= 0 && x < matr[y].length && x >= 0 && matr[y][x] == 1) {
         y += dirY;
         x += dirX;
         ++length;
+
+        if (dirY != 0 && length >= 4){
+            findBase(matr, x - dirX, y - dirY, -1, 0, length);
+            findBase(matr, x - dirX, y - dirY,  1, 0, length);
+        }
+        else if (dirY == 0 && length >= 4){
+            findBase(matr, x - dirX, y - dirY, 0, -1, length);
+            findBase(matr, x - dirX, y - dirY, 0,  1, length);
+        }
+
     }
 
-    if (dirY != 0){
-        findBase(x, y, -1, 0, length);
-        findBase(x, y,  1, 0, length);
-    }
-    else {
-        findBase(x, y,  0, -1, length);
-        findBase(x, y,  0,  1, length);
-    }
+    //console.log("Length: " + length);
 
 }
 
-function findBase(matr, x, y, dirX, dirY, lentgh){
+function findBase(matr, x, y, dirX, dirY, length){
 
-    if (matr[y + dirY] == null || matr[y + dirY][x + dirX] == null) return;
+    //console.log("x: " + x + " - y: " + y);
 
-    let bLength;
+    let bLength = 0;
 
-    while(y + dirY < matr.length && x + dirX < matr[y + dirY].length && matr[y + dirY][x + dirX] && matr[y + dirY][x + dirX] == 1) {
+    while(y < matr.length && y >= 0 && x < matr[y].length && x >= 0 && matr[y][x] && matr[y][x] == 1) {
         y += dirY;
         x += dirX;
         ++bLength;
-    }
 
-    console.log("Arrivato a qualcosa");
-    if (length == 2 * bLength) ++cont;
+        //console.log("bLength: " + bLength);
+
+        if (length == 2 * bLength) {
+            console.log("Arrivato a qualcosa");
+            ++cont;
+            return true;
+        }
+
+    }
 
 }
 
